9df2a2d523a79c5cacea3093a0fc0ab447325aa1
Jannie Theron <jannie@flatcircle.io>
2017-03-20 11:14:28 +0200

    public func

--- FlatUI/UIViewExtensions.swift
+++ FlatUI/UIViewExtensions.swift
@@ -4,7 +4,7 @@ import UIKit
 let rootViewSwapDuration = 0.68
 
 extension UIView {
-    class func animateRootSwap(from window: UIWindow, to viewController: UIViewController) {
+    public class func animateRootSwap(from window: UIWindow, to viewController: UIViewController) {
         UIView.transition(from: window.rootViewController!.view, to: viewController.view, duration: rootViewSwapDuration, options: [.transitionCrossDissolve], completion: {
             _ in
             window.rootViewController = viewController
