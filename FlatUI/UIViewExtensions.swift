a834649113faaf94bb59231f6c820ced9d51c94d
Jannie Theron <jannie@flatcircle.io>
2017-03-22 15:00:42 +0200

    added completion block

--- FlatUI/UIViewExtensions.swift
+++ FlatUI/UIViewExtensions.swift
@@ -4,10 +4,11 @@ import UIKit
 let rootViewSwapDuration = 0.68
 
 extension UIView {
-    public class func animateRootSwap(from window: UIWindow, to viewController: UIViewController) {
+    public class func animateRootSwap(from window: UIWindow, to viewController: UIViewController, completion: @escaping () -> Void) {
         UIView.transition(from: window.rootViewController!.view, to: viewController.view, duration: rootViewSwapDuration, options: [.transitionCrossDissolve], completion: {
             _ in
             window.rootViewController = viewController
+            completion()
         })
     }
 }
